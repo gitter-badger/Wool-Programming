ce267374a19c2fd034b8ec28edacdc335d97f3c9
Syouta Hirama <vghjjj@gmail.com>
2015-02-15 21:49:02 +0900

    calculator

---
+++ C#/ito/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ito
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.Calc = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox1.Location = new System.Drawing.Point(13, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(121, 45);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox2.Location = new System.Drawing.Point(211, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(121, 45);
+			this.textBox2.TabIndex = 1;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "＋",
+            "－",
+            "×",
+            "÷"});
+			this.comboBox1.Location = new System.Drawing.Point(140, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(65, 45);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(338, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(26, 27);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "=";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.textBox3.Location = new System.Drawing.Point(370, 12);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 45);
+			this.textBox3.TabIndex = 4;
+			// 
+			// Calc
+			// 
+			this.Calc.Location = new System.Drawing.Point(12, 72);
+			this.Calc.Name = "Calc";
+			this.Calc.Size = new System.Drawing.Size(460, 79);
+			this.Calc.TabIndex = 5;
+			this.Calc.Text = "計算する";
+			this.Calc.UseVisualStyleBackColor = true;
+			this.Calc.Click += new System.EventHandler(this.Calc_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("MS UI Gothic", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label2.Location = new System.Drawing.Point(12, 187);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 34);
+			this.label2.TabIndex = 6;
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 261);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.Calc);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "計算機";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button Calc;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ C#/ito/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ito
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Calc_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (comboBox1.SelectedIndex == 0)
+				{
+					double input1 = double.Parse(textBox1.Text);
+					double input2 = double.Parse(textBox2.Text);
+					double ans = input1 + input2;
+					textBox3.Text = ans.ToString();
+				}
+				else if (comboBox1.SelectedIndex == 1)
+				{
+					double input1 = double.Parse(textBox1.Text);
+					double input2 = double.Parse(textBox2.Text);
+					double ans = input1 - input2;
+					textBox3.Text = ans.ToString();
+				}
+				else if (comboBox1.SelectedIndex == 2)
+				{
+					double input1 = double.Parse(textBox1.Text);
+					double input2 = double.Parse(textBox2.Text);
+					double ans = input1 * input2;
+					textBox3.Text = ans.ToString();
+				}
+				else if (comboBox1.SelectedIndex == 3)
+				{
+					double input1 = double.Parse(textBox1.Text);
+					double input2 = double.Parse(textBox2.Text);
+					double ans = input1 / input2;
+					textBox3.Text = ans.ToString();
+				}
+				label2.Text = "";
+			}
+			catch (System.FormatException ex)
+			{
+				label2.Text = "数値を入力してください";
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.SelectedIndex = 0;
+		}
+	}
+}
