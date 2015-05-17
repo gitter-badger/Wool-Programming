c06af61aa9215dd2956d5ed18640e7e6ce3344e3
Syouta Hirama <vghjjj@gmail.com>
2015-05-18 05:46:26 +0900

    takamiyanasuno

---
+++ C#/takamiyanasuno/takamiyanasuno/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace takamiyanasuno
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void showpresen()
+		{
+			MessageBox.Show("「高宮なすのです！」\nBlu-ray１巻\n8/28 発売");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			showpresen();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			showpresen();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			showpresen();
+		}
+	}
+}
