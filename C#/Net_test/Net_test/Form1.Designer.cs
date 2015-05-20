eeb22e0d939f8f2015e70b3b8b86b0e6dc8a43a1
Syouta Hirama <vghjjj@gmail.com>
2015-05-20 18:30:11 +0900

    Wi-Fiのいろいろ取得

---
+++ C#/Net_test/Net_test/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Net_test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 35);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(359, 185);
+			this.textBox1.TabIndex = 1;
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 226);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(119, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "START";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(133, 226);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(119, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "STOP";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(253, 226);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(119, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "CLOSE";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(13, 13);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 16);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 261);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ C#/Net_test/Net_test/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace Net_test
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void getDefaultGateway()
+		{
+			NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+			foreach (NetworkInterface nic in adapters)
+			{
+				IPInterfaceProperties ipInfo = nic.GetIPProperties();
+				GatewayIPAddressInformationCollection gatewayaddress = ipInfo.GatewayAddresses;
+				IPAddressCollection dnsadd = ipInfo.DnsAddresses;
+				IPAddressCollection dhcpadd = ipInfo.DhcpServerAddresses;
+				string netname = nic.Name.ToString();
+
+				if (netname.Contains("Wi-Fi"))
+				{
+					textBox1.AppendText("名称		: " + nic.Name + Environment.NewLine);
+					textBox1.AppendText("説明		: " + nic.Description + Environment.NewLine);
+					textBox1.AppendText("I/F種類		: " + nic.NetworkInterfaceType.ToString() + Environment.NewLine);
+					textBox1.AppendText("最大速度		: " + (nic.Speed / 1000000).ToString() + "Mbps" + Environment.NewLine);
+					textBox1.AppendText("macアドレス	: " + nic.GetPhysicalAddress().ToString() + Environment.NewLine);
+					textBox1.AppendText("送信byte数	: " + nic.GetIPv4Statistics().BytesSent.ToString() + Environment.NewLine);
+					textBox1.AppendText("受信byte数	: " + nic.GetIPv4Statistics().BytesReceived.ToString() + Environment.NewLine);
+					textBox1.AppendText("IPアドレス		: " + ipInfo.UnicastAddresses[1].Address.ToString() + Environment.NewLine);
+					foreach (GatewayIPAddressInformation gaway in gatewayaddress)
+					{
+						textBox1.AppendText("デフォルトゲートウェイ	: " + gaway.Address.ToString() + Environment.NewLine);
+					}
+					foreach (IPAddress ipip in dnsadd)
+					{
+						textBox1.AppendText("IPv4 DNSサーバ	: " + ipip.ToString() + Environment.NewLine);
+					}
+					foreach (IPAddress iipp in dhcpadd)
+					{
+						textBox1.AppendText("IPv4 DHCPサーバ	: " + iipp.ToString() + Environment.NewLine);
+					}
+					//textBox1.AppendText("-----------------------------------" + Environment.NewLine);
+					//string hostname = Dns.GetHostName();
+					//IPAddress[] hostip = Dns.GetHostAddresses(hostname);
+					//IPHostEntry hostentry = Dns.GetHostEntry(hostname);
+					//foreach (IPAddress ip in hostip)
+					//{
+					//	textBox1.AppendText(": " + ip.ToString()+Environment.NewLine);
+					//}
+				}
+			}
+		}
+
+		private void GetIpCollec()
+		{
+			NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+			foreach (NetworkInterface nic in adapters)
+			{
+				IPInterfaceProperties ipInfo = nic.GetIPProperties();
+				GatewayIPAddressInformationCollection gatewayaddress = ipInfo.GatewayAddresses;
+				IPAddressCollection dnsadd = ipInfo.DnsAddresses;
+				IPAddressCollection dhcpadd = ipInfo.DhcpServerAddresses;
+				string netname = nic.Name.ToString();
+
+				if (netname.Contains("Wi-Fi"))
+				{
+					textBox1.AppendText("名称		: " + nic.Name + Environment.NewLine);
+					textBox1.AppendText("説明		: " + nic.Description + Environment.NewLine);
+					textBox1.AppendText("I/F種類		: " + nic.NetworkInterfaceType.ToString() + Environment.NewLine);
+					textBox1.AppendText("最大速度		: " + (nic.Speed / 1000000).ToString() + "Mbps" + Environment.NewLine);
+					textBox1.AppendText("macアドレス	: " + nic.GetPhysicalAddress().ToString() + Environment.NewLine);
+					textBox1.AppendText("IPv4アドレス	: " + ipInfo.UnicastAddresses[1].Address.ToString() + Environment.NewLine);
+					textBox1.AppendText("IPv6アドレス	: " + ipInfo.UnicastAddresses[0].Address.ToString() + Environment.NewLine);
+					foreach (GatewayIPAddressInformation gaway in gatewayaddress)
+					{
+						textBox1.AppendText("デフォルトゲートウェイ	: " + gaway.Address.ToString() + Environment.NewLine);
+					}
+					foreach (IPAddress ipip in dnsadd)
+					{
+						textBox1.AppendText("IPv4 DNSサーバ	: " + ipip.ToString() + Environment.NewLine);
+					}
+					foreach (IPAddress iipp in dhcpadd)
+					{
+						textBox1.AppendText("IPv4 DHCPサーバ	: " + iipp.ToString() + Environment.NewLine);
+					}
+				}
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			textBox1.Clear();
+			getDefaultGateway();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (!checkBox1.Checked)
+				timer1.Start();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			timer1.Stop();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+			{
+				timer1.Stop();
+				textBox1.Clear();
+				GetIpCollec();
+			}
+		}
+	}
+}
