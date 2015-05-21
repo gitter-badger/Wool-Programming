c6d5221fb4f9b4953dbd6465095d70642e4e221c
Syouta Hirama <vghjjj@gmail.com>
2015-05-21 18:06:39 +0900

    ブラウザ(作り途中)

---
+++ C#/Browser_test/Browser_test/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Browser_test
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.URLtext = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button4 = new System.Windows.Forms.Button();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(970, 647);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+			this.webBrowser1.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+			this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+			// 
+			// URLtext
+			// 
+			this.URLtext.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.URLtext.Location = new System.Drawing.Point(256, 13);
+			this.URLtext.Name = "URLtext";
+			this.URLtext.Size = new System.Drawing.Size(681, 19);
+			this.URLtext.TabIndex = 1;
+			this.URLtext.KeyDown += new System.Windows.Forms.KeyEventHandler(this.URLtext_KeyDown);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 11);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(71, 21);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "←";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(94, 11);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(73, 21);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "→";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(175, 11);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 21);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "更新";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Location = new System.Drawing.Point(12, 38);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(984, 679);
+			this.tabControl1.TabIndex = 5;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.webBrowser1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(976, 653);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button4.Location = new System.Drawing.Point(943, 13);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(53, 19);
+			this.button4.TabIndex = 6;
+			this.button4.Text = "GO";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1008, 729);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.URLtext);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.TextBox URLtext;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
---
+++ C#/Browser_test/Browser_test/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Browser_test
+{
+	public partial class Form1 : Form
+	{
+		private string first_url = "https://www.google.co.jp/?gws_rd=ssl";
+		public Form1()
+		{
+			InitializeComponent();
+			webBrowser1.Navigate(first_url);
+		}
+
+		private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+		{
+			tabPage1.Text = "読み込み中";
+		}
+
+		private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+		{
+			URLtext.Text = webBrowser1.Url.ToString();
+			tabPage1.Text = webBrowser1.DocumentTitle;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			webBrowser1.GoBack();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			webBrowser1.GoForward();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			webBrowser1.Refresh();
+		}
+
+		private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+		{
+			WebBrowser wb = (WebBrowser)sender;
+			webBrowser1.Navigate(wb.StatusText);
+			e.Cancel = true;
+		}
+
+		private void URLtext_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				webBrowser1.Navigate(URLtext.Text);
+			}
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			webBrowser1.Navigate(URLtext.Text);
+		}
+	}
+}
